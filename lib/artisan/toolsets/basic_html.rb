17490333e9ac27be6f78b6a418c23d4b07a8261f
Rolf Timmermans <r.timmermans@voormedia.com>
2011-03-30 23:45:09 +0200

    Include title in basic HTML.

--- lib/artisan/toolsets/basic_html.rb
+++ lib/artisan/toolsets/basic_html.rb
@@ -1,6 +1,6 @@
 module Artisan
   basic_elements = %w{body div em h1 h2 h3 h4 h5 h6 head html il nav p pre
-  small span strong sub summary sup table td th tr ul}
+  small span strong sub summary sup table td th title tr ul}
 
   basic_empty_elements = %w{br}
 
