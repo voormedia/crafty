56ec2b75bc78026a77b3ab7fdea57120d13893b7
Rolf Timmermans <r.timmermans@voormedia.com>
2011-03-31 18:55:32 +0200

    Image is an empty tag.

--- lib/artisan/toolsets/basic_html.rb
+++ lib/artisan/toolsets/basic_html.rb
@@ -1,8 +1,8 @@
 module Artisan
-  basic_elements = %w{a body div em h1 h2 h3 h4 h5 h6 head html il img nav p
+  basic_elements = %w{a body div em h1 h2 h3 h4 h5 h6 head html il nav p
   pre small span strong sub summary sup table td th title tr ul}
 
-  basic_empty_elements = %w{br}
+  basic_empty_elements = %w{br img}
 
   BasicHTML = Toolset.create(basic_elements, basic_empty_elements)
 end
